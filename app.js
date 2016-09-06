84a0823de1beb7b89285b44c178872ce57686b5a
Daniel Noon <supercookies@outlook.com>
2016-09-06 18:58:46 -0500

    More coolness

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = express()
 
 app.use(express.static(__dirname + "/public"))
 
-app.listen(process.env.PORT || 8080)
+var port = process.env.PORT || 8080
+
+app.listen(port, function() {
+  console.log(port)
+})
